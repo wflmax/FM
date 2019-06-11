5d59dba3473fd682e53be2bc807fa4447523f8cd
yuqibin <930174189@qq.com>
2019-06-11 09:28:19 +0800

    提交一波proxyTable配置

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://www.abc.com',  //目标接口域名
+        changeOrigin: true,  //是否跨域
+        pathRewrite: {
+          '^/api': '/api'   //重写接口
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: '192.168.1.109',
